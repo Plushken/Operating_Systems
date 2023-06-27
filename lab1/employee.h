079a5f94c8f85f800e1a1a03ecde5c7326d09487
Plushken <96660351+Plushken@users.noreply.github.com>
2023-06-27 03:53:49 +0300

    add header

---
+++ lab1/employee.h
@@ -0,0 +1,14 @@
+//
+// Created by Rodion Ravinsky on 27.06.23.
+//
+
+#ifndef LAB1_EMPLOYEE_H
+#define LAB1_EMPLOYEE_H
+
+struct employee {
+    int num;
+    char name[10];
+    double hours;
+};
+
+#endif //LAB1_EMPLOYEE_H
